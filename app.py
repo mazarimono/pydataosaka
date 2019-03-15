c576b88f51360487132122abdd04a436f55d27fc
hoga <hogawa098@gmail.com>
2019-03-15 15:20:33 +0900

    page 3 done

--- app.py
+++ app.py
@@ -39,27 +39,34 @@ app.layout = html.Div(children=[
                 html.Div([
                     html.H1('20190324 可視化特集会', style= {'marginRight': '2%',}),
                     html.H1('合同会社 長目　CEO  小川　英幸', style={'marginRight': '2%', })
-                ], style={'marginTop':'15%', 'textAlign': 'right', 'color': 'green'})
+                ], style={'marginTop':'15%', 'textAlign': 'right', 
+                })
             ]),
         dcc.Tab(label="DATA2", value='DATA2', style=tab_style, selected_style=tab_selected_style,
             children=[
                 html.Div([
                     html.H3('自己紹介', style={'textAlign': 'Center', 'fontSize':'3rem'}),
                     html.Div([
-                    html.Img(src="https://cdn-ak.f.st-hatena.com/images/fotolife/m/mazarimono/20190315/20190315143003.png", style={'marginLeft': '5%', 'display': 'inline-block'}),
+                    html.Img(src="https://cdn-ak.f.st-hatena.com/images/fotolife/m/mazarimono/20190315/20190315143003.png" , style={'marginTop':'5%', 'marginLeft': '5%', 'display': 'inline-block', 'height': 500}),
                     html.Div([
-                    html.H3('小川　英幸'),
+                    html.H3('ひでやん'),
                     html.H3('@ogawahideyuki'),
                     html.H3('はんなりPythonの会、Crypto Kitchenのオーガナイザー'),
                     html.H3('合同会社 長目（ちょうもく）　経営'),
-                    html.H3('金融・データ・ブロックチェーン'),
-                    ], style={'display': 'inline-block', 'fontSize': '1.5rem', 'marginLeft': '2%'})
-                    ])
+                    html.H3('金融・データ・ブロックチェーンを扱う会社'),
+                    html.H3('何事も全力でをモットーに')
+                    ], style={'display': 'inline-block', 'fontSize': '2rem', 'marginLeft': '2%', 'color':'limegreen'})
+                    ], style = {'background': '#EEFFDD'})
                 ])
             ]),
         dcc.Tab(label="DATA3", value='DATA3', style=tab_style, selected_style=tab_selected_style,
             children=[
-
+                html.H3('今日話すこと', style={'textAlign': 'Center', 'fontSize':'3rem'}),
+                html.Div([
+                    html.H3('１．データを見るのって重要ですよねぇ'),
+                    html.H3('２．データをみんなで見るのにDash良いですよ'),
+                    html.H3('３．じゃあそのDashってどうやって使うの？')
+                ], style={'textAlign': 'Center', 'fontSize':'3rem', 'marginTop': '10%', 'background': '#EEFFDD', 'color':'limegreen'})
             ]),
     ], style=tabs_styles)
 ])
